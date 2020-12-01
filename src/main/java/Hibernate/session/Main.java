8cf65031123d17bd029ae442c60f002c513f5c4b
Frostetsky <nesterenko-nikita@bk.ru>
2020-12-01 03:14:29 +0300

    Получение Java объектов из БД

--- src/main/java/Hibernate/session/Main.java
+++ src/main/java/Hibernate/session/Main.java
@@ -4,6 +4,7 @@ import Hibernate.entity.Employee;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,15 +14,26 @@ public class Main {
                 .buildSessionFactory();
 
         Session session = factory.getCurrentSession();
-
-        Employee employee = new Employee("Andrey", "Borchenko", "IT", 150_000);
-
-        Main.save(employee, session);
+        getAll(session).forEach(System.out::println);
     }
 
     private static void save(Employee employee, Session session) {
         session.beginTransaction();
         session.save(employee);
         session.getTransaction().commit();
+    }
+
+    private static Employee get(int id, Session session) {
+        session.beginTransaction();
+        Employee employee = session.get(Employee.class, id);
+        session.getTransaction().commit();
+        return employee;
+    }
+
+    private static List<Employee> getAll(Session session) {
+        session.beginTransaction();
+        List<Employee> result = session.createQuery("from Employee").getResultList();
+        session.getTransaction().commit();
+        return result;
     }
 }
